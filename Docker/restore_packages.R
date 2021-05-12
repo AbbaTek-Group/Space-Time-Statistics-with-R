4696144ce38bb384b0269998e44d9915a257848f
Ivo Arrey <arreyves@DESKTOP-OJH2K3C.localdomain>
2021-05-12 18:27:07 +0200

    Dockerfile

---
+++ Docker/restore_packages.R
@@ -0,0 +1,20 @@
+
+################################### 
+# store_packages.R
+#
+# stores a list of your currently installed packages
+# uncomment to generate '.rda file'
+
+#tmp = installed.packages()
+
+#installedpackages = as.vector(tmp[is.na(tmp[,"Priority"]), 1])
+#save(installedpackages, file = 'installed_packages.rda')
+
+####################################
+# restore_packages.R
+#
+# installs each package from the stored list of packages
+
+load("/home/analysis/installed_packages.rda")
+
+for (count in 1:length(installedpackages)) install.packages(installedpackages[count])
